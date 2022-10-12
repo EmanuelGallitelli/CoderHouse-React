ff9dc17ee9474921b7229bf83797c73cf311ea52
Emanuel Gallitelli <emanuelgallitelli@gmail.com>
2022-10-12 02:01:02 -0300

    PreEntrega1 Gallitelli

--- src/layouts/PrincipalNav.jsx
+++ src/layouts/PrincipalNav.jsx
@@ -1,12 +1,13 @@
 import {Container,Nav,Navbar,NavDropdown} from 'react-bootstrap';
 import { Outlet, Link } from 'react-router-dom';
+import { GrGamepad } from 'react-icons/gr';
 
 export function PrincipalNav() {
     return (
         <>
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand as={Link} to="/">Play-Box</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/"><GrGamepad/> Play-Box</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
